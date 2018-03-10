e3cd3a31e0f572de79df4da29a1c97f9c9f76748
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-03-09 22:19:02 -0800

    Revert "Add indeterminate Checkboxes"
    This reverts commit d828932bb2cfd846b0be4971dcd7af5581d0f079.

--- scripts.js
+++ scripts.js
@@ -46,20 +46,3 @@ $('input[type="checkbox"]').change(function(e) {
 
   checkSiblings(container);
 });
-
-
-var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
-    $checkboxes = $(":checkbox");
-
-$checkboxes.on("change", function(){
-  $checkboxes.each(function(){
-    checkboxValues[this.id] = this.checked;
-  });
-
-  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
-});
-
-// On page load
-$.each(checkboxValues, function(key, value) {
-  $("#" + key).prop('checked', value);
-});
